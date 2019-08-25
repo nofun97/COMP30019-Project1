aeb568e3ec3314a9b7c1a72c35d767aadd8c37ba
Patrick Setiawan <seti4w4np4trick@gmail.com>
2019-08-25 16:49:08 +1000

    added camera control

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float cameraSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the camera based on its direction and the WASD keys, including diagonal inputs
+        if (Input.GetAxisRaw("Horizontal") >= 0) {
+            this.transform.position = transform.position + Camera.main.transform.right * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetAxisRaw("Horizontal") <= 0) {
+            this.transform.position = transform.position - Camera.main.transform.right * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetAxisRaw("Vertical") >= 0) {
+            this.transform.position = transform.position + Camera.main.transform.forward * cameraSpeed * Time.deltaTime;
+        }
+        if (Input.GetAxisRaw("Vertical") <= 0) {
+            this.transform.position = transform.position - Camera.main.transform.forward * cameraSpeed * Time.deltaTime;
+        }
+    }
+}
