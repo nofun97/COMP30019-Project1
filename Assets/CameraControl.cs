c8d70eae4f2d0e918161ee7404262dcbba82fde3
Patrick Setiawan <seti4w4np4trick@gmail.com>
2019-08-26 00:38:20 +1000

    new camera control

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public float cameraSpeed = 1.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    public float yawSpeed = 500.0f;
+    public float pitchSpeed = 500.0f;
 
     // Update is called once per frame
     void Update()
     {
-        // Move the camera based on its direction and the WASD keys, including diagonal inputs
+        // Move the camera based on its direction and the WASD keys, incuding diagonal movements
         if (Input.GetAxisRaw("Horizontal") >= 0) {
             this.transform.position = transform.position + Camera.main.transform.right * cameraSpeed * Time.deltaTime;
         }
@@ -22,5 +26,10 @@ public class CameraControl : MonoBehaviour
         if (Input.GetAxisRaw("Vertical") <= 0) {
             this.transform.position = transform.position - Camera.main.transform.forward * cameraSpeed * Time.deltaTime;
         }
+
+        // Change the pitch and yaw of the camera based on the mouse movements
+        yaw += yawSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+        pitch -= pitchSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
